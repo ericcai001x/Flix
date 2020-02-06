2be31db8961048ea79b9e9580b3ede909d5560a5
Eric <ericcai@hotmail.com>
2020-02-06 01:58:41 -0800

    part 2

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Eric Cai on 2/5/20.
+//  Copyright © 2020 Eric Cai. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
